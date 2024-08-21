6f7bbd9894598f33d3db8774048bb83af960bfa5
Shamika Redkar <shamikaredkar91@gmail.com>
2024-08-21 14:54:08 -0400

    Added logic using useRef()

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  //to know when the timer expired
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const timer = useRef();
+  function handleStart() {
+    setTimeout(() => {
+      timer = setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer);
+  }
+
   return (
     <section className='challenge'>
       <h2>{title}</h2>
+      {timerExpired && <p>You lost</p>}
       <p className='challenge-time'>
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"}Challenge
+        </button>
       </p>
-      <p className=''>Time is running</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running" : "Timer inactive"}
+      </p>
     </section>
   );
 }
