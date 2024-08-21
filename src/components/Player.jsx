1bd2c02c676df5e7c50ff51ca5fec39356b05719
Shamika Redkar <shamikaredkar91@gmail.com>
2024-08-21 13:48:17 -0400

    Allowing user to set their own name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [name, setName] = useState("unknown entity");
+  function handleClick() {
+    setName(playerName.current.value);
+  }
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* Input is now connected to the useRef */}
+        <input ref={playerName} type='text' required />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TimerChallenge() {
+  return <div>TimerChallenge</div>;
+}
